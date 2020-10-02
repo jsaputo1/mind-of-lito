4d8de99bf2ca53c33da17706c7fc7ed6c3421b36
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-02 18:40:52 -0400

    changed while to custom foreach loop with cat filter of 1

--- themes/mindOfLito/index.php
+++ themes/mindOfLito/index.php
@@ -5,30 +5,32 @@
 <?php include ('nav.php');?>
 
 <!-- Hero Section -->
+<section class="hero">
+    <h1>Carlos Marion</h1>
+    <h2>Video Editor</h1>
+</section>
 
 <!-- About Section -->
 
-<?php dynamic_sidebar ('about')?>
+<?php include ('page-about.php');?>
 
 <!-- Video Section -->
-
-<?php if( have_posts() ) :
-
-    while( have_posts() ) :
-        the_post(); ?>
-    
+    <?php
+        $args = array( 
+            'post_type' => 'post', 
+            'cat' => '2', 
+            'order' => 'ASC',
+            'numberposts' => 3
+            );
+        $product_posts = get_posts( $args ); 
+    ?>
+    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
     <h2><?php the_title(); ?></h2>
     <h3><?php the_permalink();?></h3>
     <?php the_content(); ?>
     
     <!-- Loop ends -->
-    <?php endwhile;?>
-
-<?php the_posts_navigation();?>
-
-<?php else : ?>
-        <p>No posts found</p>
-<?php endif;?>
+    <?php endforeach; wp_reset_postdata(); ?>
 
 <!-- Contact Section -->
 
