19bd66dcf0b709be3a447bf5914151ae1d285a0e
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-02 17:27:02 -0400

    added HTML shell for nav menu

---
+++ themes/mindOfLito/nav.php
@@ -0,0 +1,8 @@
+<nav>
+  <ul>
+    <li><a href="#home">Home</a>
+    <li><a href="#about">About</a></li>
+    <li><a href="#portfolio">Portfolio</a></li>
+    <li><a href="#contact">Contact</a></li>
+</ul>
+</nav>
