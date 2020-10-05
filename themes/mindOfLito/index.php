56bb05157ac60eb21701245861cb809289d218ce
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-05 19:14:28 -0400

    added portfolio/about titles, thumbnails for posts

--- themes/mindOfLito/index.php
+++ themes/mindOfLito/index.php
@@ -11,6 +11,7 @@
 </section>
 
 <!-- About Section -->
+<section class="about">
     <?php
         $args = array( 
             'post_type' => 'post', 
@@ -21,11 +22,19 @@
         $product_posts = get_posts( $args ); 
     ?>
     <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
+    <div class="about-text">
+        <h2><?php the_title(); ?></h2>
+        <h3><?php the_content(); ?></h3>
+    </div>
+    <div class="about-photo">
+        <figure><?php the_post_thumbnail(); ?></figure>
+    </div>
     <?php endforeach; wp_reset_postdata(); ?>
+</section>
 
 <!-- Video Section -->
+<h1 class="title">Portfolio</h1>
+<section class="portfolio">
     <?php
         $args = array( 
             'post_type' => 'post', 
@@ -36,10 +45,13 @@
         $product_posts = get_posts( $args ); 
     ?>
     <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-    <h2><?php the_title(); ?></h2>
-    <h3><?php the_permalink();?></h3>
-    <?php the_content(); ?>
+    <figure>
+        <?php the_post_thumbnail(); ?>
+        <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+        <?php the_content(); ?>
+    </figure>
     <?php endforeach; wp_reset_postdata(); ?>
+</section>
 
 <!-- Contact Section -->
 
