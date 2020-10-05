ee05b322b90d63760b32a26e2185c5302a5fce91
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-05 19:27:00 -0400

    added html for contact form

---
+++ themes/mindOfLito/page-contact.php
@@ -0,0 +1,14 @@
+<section class="contact">
+  <h1>Contact</h1>
+  <form>
+      <label for="name">Name</label>
+      <textarea name="name" name="firstname"></textarea>
+      <label for="email">Email</label>
+      <textarea name="email"></textarea>
+      <label for="subject">Subject</label>
+      <textarea name="subject"></textarea>
+      <label for="message">Message</label>
+      <textarea name="message"></textarea>
+      <input type="submit" value="Send" />
+  </form>
+</section>
