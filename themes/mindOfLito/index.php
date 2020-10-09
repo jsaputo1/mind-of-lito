79295dde3f6a5d69a9446674b32e1372661c7629
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-08 20:38:19 -0400

    moved portfolio contents to separate gallery page

--- themes/mindOfLito/index.php
+++ themes/mindOfLito/index.php
@@ -35,27 +35,6 @@
     <?php endforeach; wp_reset_postdata(); ?>
 </section>
 
-<!-- Video Section -->
-<h1 class="title">Portfolio</h1>
-<section class="portfolio">
-    <?php
-        $args = array( 
-            'post_type' => 'post', 
-            'cat' => '2', 
-            'order' => 'ASC',
-            'numberposts' => 3
-            );
-        $product_posts = get_posts( $args ); 
-    ?>
-    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-    <figure>
-        <a href="<?php the_permalink();?>"><?php the_post_thumbnail(); ?></a>
-        <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-        <p><?php the_excerpt() ?></p>
-    </figure>
-    <?php endforeach; wp_reset_postdata(); ?>
-</section>
-
 <!-- Contact Section -->
 <h1 class="title">Contact</h1>
 <?php include ('page-contact.php');?>
