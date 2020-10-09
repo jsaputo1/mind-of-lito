b92f27b83c184e9ead12238dfa8f9c22002c0912
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-09 16:40:26 -0400

    changed # of posts to 9 in loop. added div for text

--- themes/mindOfLito/page-gallery.php
+++ themes/mindOfLito/page-gallery.php
@@ -1,21 +1,27 @@
 <?php get_header(); ?>
-<h1 class="title">Gallery</h1>
 <section class="gallery">
-    <?php
-        $args = array( 
-            'post_type' => 'post', 
-            'cat' => '2', 
-            'order' => 'ASC',
-            'numberposts' => 3
-            );
-        $product_posts = get_posts( $args ); 
-    ?>
-    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-    <figure>
-        <a href="<?php the_permalink();?>"><?php the_post_thumbnail(); ?></a>
-        <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-        <p><?php the_excerpt() ?></p>
-    </figure>
-    <?php endforeach; wp_reset_postdata(); ?>
+<h1 class="title">Gallery</h1>
+    <div class="gallery-posts">
+        <?php
+            $args = array( 
+                'post_type' => 'post', 
+                'cat' => '2', 
+                'order' => 'ASC',
+                'numberposts' => 9
+                );
+            $product_posts = get_posts( $args ); 
+        ?>
+        <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+        <div class="card-container">
+            <figure>
+                <a href="<?php the_permalink();?>"><?php the_post_thumbnail(); ?></a>
+            </figure>
+            <div class="gallery-card-text">
+                <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
+                <p><?php the_excerpt() ?></p>
+            </div>
+        </div>
+        <?php endforeach; wp_reset_postdata(); ?>
+    </div>
 </section>
 <?php get_footer();?>
