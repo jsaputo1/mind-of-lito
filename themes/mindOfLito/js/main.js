40c5ccdfdd7f9a551b32ecfef3b824dbe609679d
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-14 20:25:48 -0400

    added hide/show for image/text

--- themes/mindOfLito/js/main.js
+++ themes/mindOfLito/js/main.js
@@ -1,3 +1,13 @@
-(function ($) {
-  console.log('working test');
-})(jQuery);
+jQuery(document).ready(function ($) {
+
+  $(".card-container").mouseenter(function () {
+    $(this).find(".gallery-card-text").show();
+    $(this).find(".card-image").hide();
+  });
+
+  $(".card-container").mouseleave(function () {
+    $(this).find(".gallery-card-text").hide();
+    $(this).find(".card-image").show();
+  });
+
+});
