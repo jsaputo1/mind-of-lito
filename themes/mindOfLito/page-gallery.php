d409b1307f6d74e941d3ba06c1c2a9dd68595907
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-15 19:54:26 -0400

    added embedded video, added meta data to card-text

--- themes/mindOfLito/page-gallery.php
+++ themes/mindOfLito/page-gallery.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
 <section class="gallery">
-<h1 class="title">Gallery</h1>
+    <h1 class="title">Gallery</h1>
+    <div class="embedded-video">
+        <iframe width="420" height="315"
+        src="https://www.youtube.com/embed/tgbNymZ7vqY" id="big-video">
+        </iframe> 
+    </div>
     <div class="gallery-posts">
         <?php
             $args = array( 
@@ -14,14 +19,18 @@
         <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
         <div class="card-container">
             <div class="card-image">
+                <?php echo ( get_post_meta( get_the_ID(), '_video-link', true ) ); ?>
                 <figure>
                     <a href="<?php the_permalink();?>"><?php the_post_thumbnail(); ?></a>
                 </figure>
             </div>
             <div class="card-hover-text">
                 <div class="gallery-card-text">
-                    <h2><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h2>
-                    <p><?php the_excerpt() ?></p>
+                    <h2 data-link="<?php echo ( get_post_meta( get_the_ID(), 'video-link', true ) ); ?>" 
+                    class="video-link"><?php the_title(); ?></h2>
+                    <?php echo ( get_post_meta( get_the_ID(), '_description', true ) ); ?>
+                    <p></p>
+                   
                 </div>
             </div>
         </div>
