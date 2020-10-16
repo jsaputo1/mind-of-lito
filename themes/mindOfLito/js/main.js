a4f4b601aa42238887148873a05763d978dc7330
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-15 20:43:19 -0400

    removed hide/show of card image

--- themes/mindOfLito/js/main.js
+++ themes/mindOfLito/js/main.js
@@ -4,12 +4,10 @@ jQuery(document).ready(function ($) {
 
   $(".card-container").mouseenter(function () {
     $(this).find(".gallery-card-text").show();
-    $(this).find(".card-image").hide();
   });
 
   $(".card-container").mouseleave(function () {
     $(this).find(".gallery-card-text").hide();
-    $(this).find(".card-image").show();
   });
 
   $('.video-link').click(function () {
