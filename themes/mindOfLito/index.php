561119546a0736856e3332bda7834fe7743f7e1e
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-18 17:02:44 -0400

    removed about section

--- themes/mindOfLito/index.php
+++ themes/mindOfLito/index.php
@@ -6,28 +6,6 @@
     <?php dynamic_sidebar ('hero')?>
 </section>
 
-<!-- About Section -->
-<section class="about" id="about">
-    <?php
-        $args = array( 
-            'post_type' => 'post', 
-            'cat' => '3', 
-            'order' => 'ASC',
-            'numberposts' => 1
-            );
-        $product_posts = get_posts( $args ); 
-    ?>
-    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-    <div class="about-text">
-        <h2><?php the_title(); ?></h2>
-        <h3><?php the_content(); ?></h3>
-    </div>
-    <div class="about-photo">
-        <figure><?php the_post_thumbnail(); ?></figure>
-    </div>
-    <?php endforeach; wp_reset_postdata(); ?>
-</section>
-
 <!-- Footer -->
     
 <?php get_footer();?>
