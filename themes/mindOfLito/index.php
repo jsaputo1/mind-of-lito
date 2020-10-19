844fc07be8bbd0e14cf9c023c8d150d071fb8843
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-10-19 19:16:01 -0400

    added container

--- themes/mindOfLito/index.php
+++ themes/mindOfLito/index.php
@@ -1,32 +1,35 @@
 <!-- Header  -->
 <?php get_header(); ?>
 
-<!-- Hero Section -->
-<section class="hero">
-    <?php dynamic_sidebar ('hero')?>
-</section>
+<div class="main-container">
 
-<!-- About Section -->
-<section class="about" id="about">
-    <?php
-        $args = array( 
-            'post_type' => 'post', 
-            'cat' => '3', 
-            'order' => 'ASC',
-            'numberposts' => 1
-            );
-        $product_posts = get_posts( $args ); 
-    ?>
-    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
-    <div class="about-text">
-        <h2><?php the_title(); ?></h2>
-        <h3><?php the_content(); ?></h3>
-    </div>
-    <div class="about-photo">
-        <figure><?php the_post_thumbnail(); ?></figure>
-    </div>
-    <?php endforeach; wp_reset_postdata(); ?>
-</section>
+    <!-- Hero Section -->
+    <section class="video">
+        <?php dynamic_sidebar ('video')?>
+    </section>
+
+    <!-- About Section -->
+    <section class="about" id="about">
+        <?php
+            $args = array( 
+                'post_type' => 'post', 
+                'cat' => '3', 
+                'order' => 'ASC',
+                'numberposts' => 1
+                );
+            $product_posts = get_posts( $args ); 
+        ?>
+        <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+        <div class="about-text">
+            <h2><?php the_title(); ?></h2>
+            <h3><?php the_content(); ?></h3>
+        </div>
+        <div class="about-photo">
+            <figure><?php the_post_thumbnail(); ?></figure>
+        </div>
+        <?php endforeach; wp_reset_postdata(); ?>
+    </section>
+</div>
 
 <!-- Footer -->
     
