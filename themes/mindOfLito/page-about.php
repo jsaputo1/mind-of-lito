2395c743340f05b390dddbf55970d0219fbd8946
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-11-04 18:32:12 -0500

    moved about section to separate page

--- themes/mindOfLito/page-about.php
+++ themes/mindOfLito/page-about.php
@@ -1,23 +1,34 @@
-<?php
-        $args = array( 
-            'post_type' => 'post', 
-            'cat' => '3', 
-            'order' => 'ASC',
-            'numberposts' => 3
-            );
-        $product_posts = get_posts( $args ); 
-    ?>
-    <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?> 
+<!-- Header  -->
+<?php get_header(); ?>
 
-<section class="about">
-  <div class="about-text">
-    <h1><?php the_title(); ?></h1>
-    <h2><?php the_content(); ?></h1>
-  </div>
-  <div class="about-photo">
-    <figure><?php the_post_thumbnail(); ?></figure>
-  </div>
-</section>
+<div class="main-container">
 
-    <!-- Loop ends -->
-    <?php endforeach; wp_reset_postdata(); ?>
+  <!-- About Section -->
+   <section class="about">
+      <?php
+          $args = array( 
+              'post_type' => 'post', 
+              'cat' => '3', 
+              'order' => 'ASC',
+              'numberposts' => 1
+              );
+          $product_posts = get_posts( $args ); 
+      ?>
+      <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+      <div class="about-text">
+          <div class="about-title">
+              <h2><?php the_title(); ?></h2>
+              <h4><?php the_excerpt(); ?></h4>
+          </div>
+          <h3><?php the_content(); ?></h3>
+      </div>
+      <div class="about-photo">
+          <figure><?php the_post_thumbnail(); ?></figure>
+      </div>
+      <?php endforeach; wp_reset_postdata(); ?>
+  </section>
+  <!-- End About Section -->
+</div>
+
+<!-- Footer -->
+<?php get_footer();?>
