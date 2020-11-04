a48f9ce81dec37e9012d8f9c4ec7cb709b3c15c5
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-11-04 18:44:08 -0500

    removed loop, changed to ACF field

--- themes/mindOfLito/page-about.php
+++ themes/mindOfLito/page-about.php
@@ -5,27 +5,16 @@
 
   <!-- About Section -->
    <section class="about">
-      <?php
-          $args = array( 
-              'post_type' => 'post', 
-              'cat' => '3', 
-              'order' => 'ASC',
-              'numberposts' => 1
-              );
-          $product_posts = get_posts( $args ); 
-      ?>
-      <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
       <div class="about-text">
           <div class="about-title">
-              <h2><?php the_title(); ?></h2>
-              <h4><?php the_excerpt(); ?></h4>
+              <h2><?php the_field('about_title'); ?></h2>
+              <h4><?php the_field('about_name'); ?></h4>
           </div>
-          <h3><?php the_content(); ?></h3>
+          <h3><?php the_field('about_paragraph'); ?></h3>
       </div>
       <div class="about-photo">
           <figure><?php the_post_thumbnail(); ?></figure>
       </div>
-      <?php endforeach; wp_reset_postdata(); ?>
   </section>
   <!-- End About Section -->
 </div>
