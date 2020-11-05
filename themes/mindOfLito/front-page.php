490ddff8c5fde03801b9c5db2ce6131ceb022acb
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-11-04 19:16:05 -0500

    changed video link to ACF field

--- themes/mindOfLito/front-page.php
+++ themes/mindOfLito/front-page.php
@@ -4,7 +4,11 @@
 <div class="main-container">
     <!-- Video Section -->
     <section class="video">
-        <?php dynamic_sidebar ('video')?>
+        <div class="front-page-video">
+            <video src="<?php the_field('front_page_video'); ?>" 
+            poster="<?php the_field('front_page_video_thumbnail'); ?>" controls>
+            ></video>
+        </div>
     </section>
     <!-- End Video Section -->
 </div>
