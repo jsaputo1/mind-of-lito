cd1812cfd9b1f9d1e1082c8cbf1d532566f290d0
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-11-05 19:56:20 -0500

    moved about card into its own div

--- themes/mindOfLito/page-about.php
+++ themes/mindOfLito/page-about.php
@@ -4,18 +4,20 @@
 <div class="main-container">
 
   <!-- About Section -->
-   <section class="about">
-      <div class="about-text">
-          <div class="about-title">
-              <h2><?php the_field('about_name'); ?></h2>
-              <h4><?php the_field('about_title'); ?></h4>
-          </div>
-          <h3><?php the_field('about_paragraph'); ?></h3>
-      </div>
-      <div class="about-photo">
-          <figure><?php the_post_thumbnail(); ?></figure>
-      </div>
-  </section>
+  <section class="about">
+    <div class="about-box">
+        <div class="about-text">
+            <div class="about-title">
+                <h2><?php the_field('about_name'); ?></h2>
+                <h4><?php the_field('about_title'); ?></h4>
+            </div>
+            <h3><?php the_field('about_paragraph'); ?></h3>
+        </div>
+        <div class="about-photo">
+            <figure><?php the_post_thumbnail(); ?></figure>
+        </div>
+    </div>
+</section>
   <!-- End About Section -->
 </div>
 
