bf410a9b283f3892d290e7d9d337bc8d1c948457
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-11-06 17:22:56 -0500

    added jquery to change video description and title

--- themes/mindOfLito/js/main.js
+++ themes/mindOfLito/js/main.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
 
+  console.log('working');
+
   $(".gallery-image-text").hide();
 
   $(".card-container").mouseenter(function () {
@@ -14,8 +16,21 @@ jQuery(document).ready(function ($) {
 
   $('.video-link').click(function () {
     let newLink = $(this).attr('data-link');
-    console.log("new link", newLink);
+    let newTitle = $(this).attr('title');
+    let newDescription = $(this).attr("description");
+    let newThumbnail = $(this).attr('thumbnail-link');
+
+    // console.log("new link", newLink);
+    // console.log("new Title", newTitle);
+    // console.log("new Description", newDescription);
+    // console.log("new Thumbnail", newThumbnail);
+
     $('#big-video').attr('src', newLink);
+    $('#big-video-title').html(newTitle);
+    $('#video-description').html(newDescription);
+    $('#video-description-thumbnail').attr('src', newThumbnail);
+
+
   });
 
 
