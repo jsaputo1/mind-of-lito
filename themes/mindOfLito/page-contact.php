b321a0e6e9c38ba324e8a34305823871b52c03fa
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2020-11-11 19:50:43 -0500

    fixed ACF path

--- themes/mindOfLito/page-contact.php
+++ themes/mindOfLito/page-contact.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <section class="contact">
-   <h1 class="title"><?php the_field('gallery_big_video_description'); ?></h1>
+   <h1 class="title"><?php the_field('contact_title'); ?></h1>
    <form class="contact-form row">
       <?php the_content(); ?>
    </form>
